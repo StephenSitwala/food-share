cea1912de0327e4c6fcfc062138c80883bbf2752
STEPHEN SITWALA <Stephensitwala09@gmail.com>
2026-08-09 23:03:33 -1100

    changes on index.html and menu card

--- scripts/app.js
+++ scripts/app.js
@@ -143,19 +143,19 @@ const defaultMenu =[
     //pizza & plasta
     //=============
      {
-        name: "pizza two one",
+        name: "pizza two in one",
         price: 78,
         image:"photos/pizza.jpg",
         category:"pizza & plasta"
     },
     {
-        name: "pizza plasta",
+        name: "pizza pasta",
         price: 78,
         image:"photos/images p(25).jpeg",
         category:"pizza & plasta"
     },
     {
-        name: "pizza plast with mixed vegatables",
+        name: "pizza past with mixed vegatables",
         price: 78,
          image:"photos/images p(26).jpeg",
         category:"pizza & plasta"
@@ -180,13 +180,13 @@ const defaultMenu =[
         category:"pizza & plasta"
     },
     {
-        name: "pizza plasta",
+        name: "pizza pasta",
         price: 78,
         image:"photos/images p(25).jpeg",
         category:"pizza & plasta"
     },
     {
-        name: "pizza plasta",
+        name: "pizza pasta",
         price: 78,
         image:"photos/images p(25).jpeg",
         category:"pizza & plasta"
